21e568e60863f4d339e136fb7c34c394c1871980
Сергей Мосякин <smosyakin@yandex.ru>
2025-10-10 11:57:54 +0300

    update func mask card account

--- src/widget.py
+++ src/widget.py
@@ -6,19 +6,11 @@ import masks
 def mask_account_card(card_account_num: str) -> str:
     """Функция маскирующая номер счета или карты"""
     if re.search("[а-яА-Я]", card_account_num):
-        split_card_account_num = card_account_num.split()
-        mask_account_num = masks.get_mask_account(int(split_card_account_num[-1]))
-        split_card_account_num.remove(split_card_account_num[-1])
-        split_card_account_num.append(mask_account_num)
-        str_mask_account = " ".join(split_card_account_num)
-        return str_mask_account
+        mask_account_num = masks.get_mask_account(int(card_account_num[-20:]))
+        return f"{card_account_num[:-20]}{mask_account_num}"
     else:
-        split_card_account_num = card_account_num.split()
-        mask_card_num = masks.get_mask_card_number(int(split_card_account_num[-1]))
-        split_card_account_num.remove(split_card_account_num[-1])
-        split_card_account_num.append(mask_card_num)
-        str_mask_card = " ".join(split_card_account_num)
-        return str_mask_card
+        mask_card_num = masks.get_mask_card_number(int(card_account_num[-17:]))
+        return f"{card_account_num[:-17]} {mask_card_num}"
 
 
 def get_date(full_date: str) -> str:
