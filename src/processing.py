f2caf9a4fd7ff54f1d93dc08aa3791c130a62209
Сергей Мосякин <smosyakin@yandex.ru>
2025-10-16 22:23:34 +0300

    add sorting function

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,8 @@ def filter_by_state(list_of_dicts_id: list, state="EXECUTED") -> list:
             if value == state:
                 new_list_of_dicts.append(i)
     return new_list_of_dicts
+
+def sort_by_date(list_of_dicts_id: list, sorting_type=True) -> list:
+    """Функция, сортирующая словари по нужному ключу"""
+    sorted_list_of_dict = sorted(list_of_dicts_id, key=lambda dict_id: dict_id["date"], reverse=sorting_type)
+    return sorted_list_of_dict
