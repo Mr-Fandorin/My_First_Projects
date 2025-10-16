9bcaadbb6d7526d4aa8c586812aed6acada26fcd
Сергей Мосякин <smosyakin@yandex.ru>
2025-10-16 22:24:21 +0300

    fixing by black

--- src/processing.py
+++ src/processing.py
@@ -7,6 +7,7 @@ def filter_by_state(list_of_dicts_id: list, state="EXECUTED") -> list:
                 new_list_of_dicts.append(i)
     return new_list_of_dicts
 
+
 def sort_by_date(list_of_dicts_id: list, sorting_type=True) -> list:
     """Функция, сортирующая словари по нужному ключу"""
     sorted_list_of_dict = sorted(list_of_dicts_id, key=lambda dict_id: dict_id["date"], reverse=sorting_type)
